8fe2bae83747d6147356d624115f1f0bc6e326cf
Shahaf Abileah <shahaf@gmail.com>
2021-10-07 14:29:35 -0700

    Display a super simple CharityRow component with hard-coded data

---
+++ client/src/CharityRow.tsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import './CharityRow.css';
+
+function CharityRow() {
+  return (
+    <div className="CharityRow">
+        <span className="TableCell">
+            <a href="http://www.kexp.org/">Friends of Kexp</a>
+        </span>
+        <span className="TableCell">
+            88
+        </span>
+        <span className="TableCell">
+            91-2061474
+        </span>
+    </div>    
+  );
+}
+
+export default CharityRow;
